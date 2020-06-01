dee7707a9cb2cea208d2b7208765749a7a3dc6c6
Serkan Sozer <eserkansozer@hotmail.com>
2020-06-01 17:54:05 +0100

    SAD-Supply staff modal content

--- src/app/Models/AAModalModels.ts
+++ src/app/Models/AAModalModels.ts
@@ -13,13 +13,19 @@ export class AAModalModels {
       },
       {
         assessmentArea : 'Energy',
-        title : 'Spending on teaching staff as a percentage of total expenditure',
+        title : 'Spending on energy as a percentage of total expenditure',
         textContent : `<p>Schools may receive an amber or red rating on this indicator if they spend significantly more of their
         budget on energy than other schools.</p>
         <p>Schools should understand their rating and whether any changes are required to achieve better resource management.</p>
         <p>Spend that is significantly higher than other schools may suggest that too much resource is spent on energy and more conservation
         may need to be considered for both environmental and expense reasons.</p>`
+      },
+      {
+        assessmentArea : 'Supply staff',
+        title : 'Spending on supply staff as a percentage of total expenditure',
+        textContent : `<p>n/a</p>`
       }
+
     ];
   }
 }
