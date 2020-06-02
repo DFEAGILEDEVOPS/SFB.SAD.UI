3de9ec635a0a2d356f1ca262cc3c54b8da9f44be
Serkan Sozer <eserkansozer@hotmail.com>
2020-06-02 15:24:27 +0100

    SAD-New assessment areas

--- src/app/Models/AAModalModels.ts
+++ src/app/Models/AAModalModels.ts
@@ -12,18 +12,43 @@ export class AAModalModels {
         <p>Schools should understand their rating and whether any changes are required to achieve better resource management.</p>`
       },
       {
-        assessmentArea : 'Energy',
-        title : 'Spending on energy as a percentage of total expenditure',
-        textContent : `<p>Schools may receive an amber or red rating on this indicator if they spend significantly more of their
-        budget on energy than other schools.</p>
-        <p>Schools should understand their rating and whether any changes are required to achieve better resource management.</p>
-        <p>Spend that is significantly higher than other schools may suggest that too much resource is spent on energy and more conservation
-        may need to be considered for both environmental and expense reasons.</p>`
-      },
-      {
         assessmentArea : 'Supply staff',
         title : 'Spending on supply staff as a percentage of total expenditure',
         textContent : `<p>n/a</p>`
+      },
+      {
+        assessmentArea : 'Education support staff',
+        title : 'Spending on education support staff as a percentage of total expenditure',
+        textContent : `<p>n/a</p>`
+      },
+      {
+        assessmentArea : 'Administrative and clerical staff',
+        title : 'Spending on administrative and clerical staff as a percentage of total expenditure',
+        textContent : `<p>n/a</p>`
+      },
+      {
+        assessmentArea : 'Other staff costs',
+        title : 'Spending on other staff costs as a percentage of total expenditure',
+        textContent : `<p>n/a</p>`
+      },
+      {
+        assessmentArea : 'Premises costs',
+        title : 'Spending on premises costs as a percentage of total expenditure',
+        textContent : `<p>n/a</p>`
+      },
+      {
+        assessmentArea : 'Teaching resources',
+        title : 'Spending on teaching resources costs as a percentage of total expenditure',
+        textContent : `<p>n/a</p>`
+      },
+      {
+        assessmentArea : 'Energy',
+        title : 'Spending on energy costs as a percentage of total expenditure',
+        textContent : `<p>Schools may receive an amber or red rating on this indicator if they spend significantly more of their
+        budget on energy than other schools.</p>
+        <p>Schools should understand their rating and whether any changes are required to achieve better resource management.</p>
+        <p>Spend that is significantly higher than other schools may suggest that too much resource is spent on energy and more
+        conservation may need to be considered for both environmental and expense reasons.</p>`
       }
 
     ];
