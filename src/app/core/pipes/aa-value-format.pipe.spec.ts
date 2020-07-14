889ee84a9961ee9ae808da984b9fea68939083fc
Serkan Sozer <eserkansozer@hotmail.com>
2020-07-14 14:49:34 +0100

    #42403 - Pipe tests

--- src/app/core/pipes/aa-value-format.pipe.spec.ts
+++ src/app/core/pipes/aa-value-format.pipe.spec.ts
@@ -1,11 +1,24 @@
-// /* tslint:disable:no-unused-variable */
 
-// import { TestBed, async } from '@angular/core/testing';
-// import { AaValueFormatPipe } from './aa-value-format.pipe';
+import { TestBed, async } from '@angular/core/testing';
+import { AaValueFormatPipe } from './aa-value-format.pipe';
 
-// describe('Pipe: AaValueFormate', () => {
-//   it('create an instance', () => {
-//     let pipe = new AaValueFormatPipe();
-//     expect(pipe).toBeTruthy();
-//   });
-// });
+describe('Pipe: aaValueFormat', () => {
+  const pipe = new AaValueFormatPipe();
+
+  it('create an instance', () => {
+    expect(pipe).toBeTruthy();
+  });
+
+  it('transforms currencies correctly', () => {
+    expect(pipe.transform(1000, 'Average teacher cost')).toBe('£1,000.00');
+  });
+
+  it('transforms ratios correctly', () => {
+    expect(pipe.transform(10, 'Pupil to teacher ratio')).toBe(10);
+  });
+
+  it('transforms percentages correctly', () => {
+    expect(pipe.transform(0.49, null)).toBe('49%');
+  });
+
+});
